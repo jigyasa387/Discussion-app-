4dd2a465a28d3fac10c864db679749870eba8ad4
Code with Jigyasa <jigyasabansal05@gmail.com>
2021-05-16 11:06:07 +0530

    Initial commit

---
+++ code.js
@@ -0,0 +1,7 @@
+var search=document.getElementById("search");
+var subject=document.getElementById("Subject");
+var question=document.getElementById("Question");
+var submit=document.getElementById("submit");
+submit.addEventListener('click',function(){
+    
+});
